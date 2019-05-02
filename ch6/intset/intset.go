bb2396cfc7b221b813babdcdd6224e90c6a7326b
Igor Kupczyński <igor@kupczynski.info>
2019-05-02 21:44:19 +0200

    Add ch6 intset exercises

---
+++ ch6/intset/intset.go
@@ -0,0 +1,114 @@
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// IntSet is a set of small non-negative integers. Zero value is an empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for len(s.words) <= word {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// AddAll adds all elements xs to the set.
+func (s *IntSet) AddAll(xs ...int) {
+	for _, x := range xs {
+		s.Add(x)
+	}
+}
+
+// Clear removes all elements from the set
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+// Copy returns a copy of the set
+func (s *IntSet) Copy() *IntSet {
+	t := &IntSet{}
+	if s.words != nil {
+		t.words = make([]uint64, len(s.words))
+		copy(t.words, s.words)
+	}
+	return t
+}
+
+// Elems return the elements present in the set as slice.
+func (s *IntSet) Elems() []int {
+	var elems []int
+	for n, word := range s.words {
+		for bit := uint(0); bit < 64; bit++ {
+			if word&(1<<bit) != 0 {
+				elems = append(elems, 64*n+int(bit))
+			}
+		}
+	}
+	return elems
+}
+
+// Has checks if the set contains non-negative value x
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return len(s.words) > word && s.words[word]&(1<<bit) != 0
+}
+
+// Len returns the number of elements in the set
+func (s *IntSet) Len() int {
+	sum := 0
+	for _, word := range s.words {
+		for bit := uint(0); bit < 64; bit++ {
+			if word&(1<<bit) != 0 {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
+// Remove removes x form the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if len(s.words) > word {
+		s.words[word] &^= 1 << bit
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
